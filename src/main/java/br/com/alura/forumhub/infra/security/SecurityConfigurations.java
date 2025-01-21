8f46b834c45cae3190e1f6034f8cb41ea01e1d8d
Gessyca Borges <gessyca.borges@gmail.com>
2025-01-21 19:54:42 -0300

    Permitindo os usuários não logados a visualizarem os tópicos

--- src/main/java/br/com/alura/forumhub/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forumhub/infra/security/SecurityConfigurations.java
@@ -29,6 +29,7 @@ public class SecurityConfigurations {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests( req -> {
                     req.requestMatchers(HttpMethod.POST, "/login").permitAll();
+                    req.requestMatchers(HttpMethod.GET).permitAll();
                     req.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
                     req.anyRequest().authenticated();
                 })
